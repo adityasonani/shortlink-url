747929573043bd551867eac7f18807b602e466e9
Aditya Sonani <adityasonani19@gmail.com>
2021-11-20 11:40:13 +0530

    Minor changes

--- src/main/java/com/shortlink/api/services/SlugControllerService.java
+++ src/main/java/com/shortlink/api/services/SlugControllerService.java
@@ -11,44 +11,44 @@ import com.shortlink.api.repository.UrlRepository;
 
 @Service
 public class SlugControllerService {
-	
+
 	@Autowired
 	UrlRepository urlRepository;
-	
+
 	public UrlModel ShortUrlControllerService(LongUrlRequestModel body) {
 		if (urlRepository.existsByLongUrl(body.getLongUrl())) {
 			return urlRepository.findByLongUrl(body.getLongUrl());
 		}
-		
-		//Base-62
-		String code="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		int min=0, max=59;
+
+		// Base-62
+		String code = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		int min = 0, max = 59;
 		String shortUrl = "";
-		
+
 		do {
 			shortUrl = "";
-			for (int i=0; i<4; i++) {
-				int randomIndex=ThreadLocalRandom.current().nextInt(min, max + 1);
-				shortUrl+=code.charAt(randomIndex);
+			for (int i = 0; i < 4; i++) {
+				int randomIndex = ThreadLocalRandom.current().nextInt(min, max + 1);
+				shortUrl += code.charAt(randomIndex);
 			}
 		} while (urlRepository.existsByShortUrl(shortUrl));
-		
+
 		UrlModel urlModel = new UrlModel();
 		urlModel.setLongUrl(body.getLongUrl());
 		urlModel.setShortUrl(shortUrl);
 		urlRepository.save(urlModel);
 		return urlModel;
 	}
-	
+
 	public String redirectToLongUrlControllerService(String shortUrl) {
 		String redirect = "/";
 		if (urlRepository.existsByShortUrl(shortUrl)) {
 			UrlModel urlModel = urlRepository.findByShortUrl(shortUrl);
 			redirect = urlModel.getLongUrl();
-		} 
+		}
 		return redirect;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "SlugControllerService [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
